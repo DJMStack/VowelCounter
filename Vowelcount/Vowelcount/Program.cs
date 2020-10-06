f460721abeaa3608c7c2c3cbaef25088bf7f88fd
crashrz3 <67346625+crashrz3@users.noreply.github.com>
2020-10-06 17:13:01 -0400

    Add files via upload

---
+++ Vowelcount/Vowelcount/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using static System.Console;
+
+namespace Vowelcount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Please enter your phrase: ");
+            string phrase = Console.ReadLine();
+            int vowelCount = 0;
+            //string subbed = phrase.Substring(0);
+            string letter = "";
+            //WriteLine(subbed);
+            WriteLine("Length of phrase is: " + phrase.Length);
+
+
+            for (int i = 0;i < phrase.Length; i++)
+            {
+                letter = phrase.ToLower().Substring(i, 1);
+
+                if (letter.Equals("a") || letter.Equals("e") || letter.Equals("i") || letter.Equals("o") || letter.Equals("u"))
+                {
+                    vowelCount++;
+                }else if (letter.Equals("e"))
+                {
+                    vowelCount++;
+                }
+                else if (letter.Equals("i"))
+                {
+                    vowelCount++;
+                }
+                else if (letter.Equals("o"))
+                {
+                    vowelCount++;
+                }
+                else if (letter.Equals("u"))
+                {
+                    vowelCount++;
+                }
+            }
+
+            WriteLine("You have {0} vowels", vowelCount);
+
+
+
+        }
+    }
+}
